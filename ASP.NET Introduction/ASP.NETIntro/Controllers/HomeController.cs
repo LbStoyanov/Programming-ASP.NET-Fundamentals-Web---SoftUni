5b1dee9a979bcede267c851c211cad3760024d30
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-12-06 13:57:31 +0200

    ASP.NET  Intro

---
+++ ASP.NET Introduction/ASP.NETIntro/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using ASP.NETIntro.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP.NETIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Test(TestModel testModel)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET Introduction/ASP.NETIntro/Models/TestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ASP.NETIntro.Models
+{
+    public class TestModel
+    {
+        public int Id { get; set; }
+        public string Product { get; set; }
+    }
+}
