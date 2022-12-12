4bff7fe7596bc83b2d3aef25054bcb9693a37c9a
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-12-12 13:45:37 +0200

    ForumAppp

---
+++ ForumApp/ForumApp/Data/Models/Post.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static ForumApp.GlobalConstants.DataConstants.Post;
+
+namespace ForumApp.Data.Models
+{
+    [Comment("Published posts")]
+    public class Post
+    {
+        [Key]
+        [Comment("Post Identifier")]
+        public int Id { get; set; }
+
+        [Comment("Post title")]
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Comment("Post Content")]
+        [Required]
+        [MaxLength(ContentMaxLength)]
+        public string Content { get; set; } = null!;
+    }
+}
---
+++ ForumApp/ForumApp/GlobalConstants/DataConstants.cs
@@ -0,0 +1,24 @@
+﻿namespace ForumApp.GlobalConstants
+{
+    /// <summary>
+    /// Data layer constants
+    /// </summary>
+    public class DataConstants
+    {
+        /// <summary>
+        /// Post constants
+        /// </summary>
+        public static class Post
+        {
+            /// <summary>
+            /// Max title length
+            /// </summary>
+            public const int TitleMaxLength = 50;
+
+            /// <summary>
+            /// Max content length
+            /// </summary>
+            public const int ContentMaxLength = 1500;
+        }
+    }
+}
