ea2fe555134d1699d5505d452e337e88ed7342aa
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-12-12 13:56:22 +0200

    ForumApp

---
+++ ForumApp/ForumApp/Data/ForumDbContext.cs
@@ -0,0 +1,23 @@
+﻿using ForumApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Data
+{
+    public class ForumDbContext : DbContext
+    {
+        public ForumDbContext(DbContextOptions<ForumDbContext> options)
+            :base(options) 
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        DbSet<Post> Posts { get; set; } = null!;
+    }
+}
