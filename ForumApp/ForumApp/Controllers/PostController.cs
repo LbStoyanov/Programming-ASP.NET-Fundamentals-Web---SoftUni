90c205f805c58dc3352628292ff5860991752083
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-12-12 15:23:44 +0200

    ForumApp

--- ForumApp/ForumApp/Controllers/PostController.cs
+++ ForumApp/ForumApp/Controllers/PostController.cs
@@ -1,4 +1,5 @@
 ﻿using ForumApp.Data;
+using ForumApp.Data.Models;
 using ForumApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -22,7 +23,53 @@ namespace ForumApp.Controllers
                     Content= p.Content,
                 })
                 .ToListAsync();
-            return View();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var model = new PostViewModel();
+
+            ViewData["Title"] = "Add new Post";
+
+            return  View("Edit", model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(PostViewModel model)
+        {
+            
+
+            if (!ModelState.IsValid)
+            {
+                ViewData["Title"] = model.Id == 0 ? "Add new Post" : "Edit Post";
+
+                return View(model);
+            }
+
+            if (model.Id == 0)
+            {
+                this.context.Posts.Add(new Post()
+                {
+                    Title= model.Title,
+                    Content= model.Content
+                });
+            }
+            else
+            {
+                var post = await this.context.Posts.FindAsync(model.Id);
+
+                if (post != null)
+                {
+                    post.Title = model.Title;
+                    post.Content = model.Content;
+                }
+            }
+
+            await this.context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
