0c6724aa25edcb77d49154f3dd6e2ae88b69ab7e
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-01-05 07:07:47 +0200

    TaskBoardApp

---
+++ TaskBoardApp/TaskBoardApp/TaskBoardApp/Data/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using static TaskBoardApp.Data.DataConstants.User;
+
+namespace TaskBoardApp.Data.Entities
+{
+    public class User : IdentityUser
+    {
+        //public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(MaxUserFirstName)]
+        public string FirstName { get; init; } = null!;
+
+        [Required]
+        [MaxLength(MaxUserLastName)]
+        public string LastName { get; init; } = null!;
+    }
+}
